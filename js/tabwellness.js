0b044143955d6be4607e0f32e709e31bfc8a3306
Daniel Lim <dalml@protonmail.com>
2019-12-14 15:02:36 +0800

    Not so initial commit but I can already forsee myself having lesser tabs

--- js/tabwellness.js
+++ js/tabwellness.js
@@ -1,19 +1,35 @@
-// Get alert number
 // Function to get count of tabs in current window. Required for the popoup display box.
-function getCurrentWindowTabCount() {
+function countTabsPlsss() {
+    // Get the count
     chrome.tabs.query({currentWindow:true}, function(tabs) {
-        return tabs.length;
+        let currentCount = tabs.length;
+        
+        // Write it to the store
+        chrome.storage.sync.set({'currentTabCount': currentCount});
     });
 }
 
-// Save the tab counter to Chrome extension storage API
-chrome.storage.sync.set({'tabCount': getCurrentWindowTabCount()}, function() {
-    console.log('Settings saved');
-});
-
-
-
 // Get current total number of tabs
+function printDataPlsss() {
+    // Tab count
+    var whatTheFuckShouldINameThisVar = chrome.storage.sync.get(['currentTabCount'], function(items){
+        if (items.currentTabCount < 7) {
+            document.getElementById('tab-count-tm').innerText = "You healthily have " + items.currentTabCount + " tabs.";
+        } else {
+            document.getElementById('tab-count-tm').innerText = items.currentTabCount + " tabs time to close shit!!!";
+        }
+    });
+}
 
+// Do something drastic when you have too many tabs
+
+
+  
+
+
+window.onload = function() {
+    this.countTabsPlsss();
+    this.printDataPlsss();
+}
 
 // Sound an alert when you have too many tabs
